2846edabd7600509cc5b309e1fb9c77141937dab
sagargh <g4maks@gmail.com>
2014-10-27 19:23:36 +0600

    This is the variable parctice in the class.

---
+++ variables.php
@@ -0,0 +1,46 @@
+<?php
+
+print "Sundarbon Tour";
+
+print "<br>";
+
+$a = 5;
+$b = $a;
+
+$c = $b;
+
+$f = $b;
+
+//print $c;
+
+$day = "Sunday";
+
+$var = "day";
+
+$$var = "Friday";
+
+
+//print $$var;
+
+//print $var;
+
+$x = 'hello';
+$hello = 'Hello';
+$$x = 'World';
+//print ${$x};// $$hello 
+
+$yoo = a;
+$yoo = "Hunny Singh";
+
+//print $yoo;
+
+//print is_string($yoo);
+
+$x = 3.14;
+settype( $x, "integer" );
+print $x;
+
+
+print "String";
+print 'String';
+?>
